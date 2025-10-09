870543a512d4da1187f4a3478b2aa41507276395
keiichi kato <momonomono@gmail.com>
2025-10-09 20:58:58 +0900

    デプロイのためのボタン修正

--- resources/views/Slider.blade.php
+++
@@ -1,7 +0,0 @@
-<ul class="flex h-full">
-    <li class="border h-full flex flex-col">
-        <p class="text-md md-1">タイトル</p>
-        <img class="w-full flex-1 object-contain shadow mb-1" src="{{ asset('img/luffy.png') }}">
-        <p>テキストテキストテキスト</p>
-    </li>
-</ul>
